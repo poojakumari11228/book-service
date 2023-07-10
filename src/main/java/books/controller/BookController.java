844f37b9e0e8bdaa024d8833a89c318d598c0f5a
poojakumari11228 <poojakumari11228@gmail.com>
2023-07-09 21:30:03 -0500

    Initial commit

---
+++ src/main/java/books/controller/BookController.java
@@ -0,0 +1,4 @@
+package books.controller;
+
+public class BookController {
+}
---
+++ src/main/java/books/domain/Book.java
@@ -0,0 +1,9 @@
+package books.domain;
+
+
+public class Book {
+    private String isbn;
+    private String author;
+    private String title;
+    private double price;
+}
