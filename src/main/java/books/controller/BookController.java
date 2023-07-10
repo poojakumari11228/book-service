5d290e4fc223160736746be3e785914687dc4b07
poojakumari11228 <poojakumari11228@gmail.com>
2023-07-10 00:10:18 -0500

    CRUD for books

--- src/main/java/books/controller/BookController.java
+++ src/main/java/books/controller/BookController.java
@@ -1,4 +1,53 @@
 package books.controller;
 
+import books.domain.Book;
+import books.dto.Books;
+import books.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
 public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public void addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @GetMapping
+    public Books getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{isbn}")
+    public Book getBook(@PathVariable String isbn) {
+        return bookService.getBook(isbn);
+    }
+
+    @DeleteMapping("/{isbn}")
+    public void deleteBook(@PathVariable String isbn) {
+        bookService.deleteBook(isbn);
+    }
+
+    @PutMapping
+    public void updateBook(@RequestBody Book book) {
+        bookService.updateBook(book);
+    }
+
+
 }
--- src/main/java/books/domain/Book.java
+++ src/main/java/books/domain/Book.java
@@ -1,5 +1,12 @@
 package books.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@AllArgsConstructor
 public class Book {
     private String isbn;
     private String author;
